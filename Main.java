8ac5880697ff39ee16abd5d619d9c683e3affb0c
waterbitt <Oswaldo.a7t@gmail.com>
2025-01-09 17:42:57 -0800

    quad

--- Main.java
+++ Main.java
@@ -3,15 +3,10 @@ public class Main {
     public static void main(String args[])
     {
         System.out.println("Hello World Lucas");
-<<<<<<< HEAD
-
         System.out.println("Oswaldo");
         //lucas is big dawg
-=======
         System.out.println("Hello World Reese Oo");
-
         System.out.println("Hello World Richard");
 
->>>>>>> refs/remotes/origin/main
     }
 }
