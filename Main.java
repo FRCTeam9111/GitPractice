1e0a99c9f5037d74c8eca875d043ffee3186551b
Richard Juarez Dominguez <richardjuarez12@icloud.com>
2025-01-09 17:13:54 -0800

    Fixed merge conflict w/ my name

--- Main.java
+++ Main.java
@@ -4,5 +4,8 @@ public class Main {
     {
         System.out.println("Hello World Lucas");
         System.out.println("Hello World Reese Oo");
+
+        System.out.println("Hello World Richard");
+
     }
 }
